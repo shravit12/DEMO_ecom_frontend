d11eebe46e1782da9a25eb7c24c185eef7a7e103
Shravit Sharma <138794026+shravit12@users.noreply.github.com>
2026-04-02 09:46:02 +0530

    Change product images in ProductDetail component
    Updated product images in the products list with new URLs.

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -4,13 +4,13 @@ import { useCart } from "../context/CartContext";
 
 // Mock product list
 const productsList = [
-  {
+   {
     id: "1",
     title: "Stylish T-Shirt",
     price: 499,
     category: "clothing",
     description: "A very comfortable and stylish t-shirt for daily wear.",
-    image: "https://via.placeholder.com/300x300?text=T-Shirt",
+    image: "https://images.pexels.com/photos/27078903/pexels-photo-27078903.jpeg",
   },
   {
     id: "2",
@@ -18,7 +18,7 @@ const productsList = [
     price: 1499,
     category: "footwear",
     description: "Perfect shoes for running and sports activities.",
-    image: "https://via.placeholder.com/300x300?text=Shoes",
+    image: "https://images.pexels.com/photos/18066208/pexels-photo-18066208.jpeg",
   },
   {
     id: "3",
@@ -26,7 +26,7 @@ const productsList = [
     price: 2999,
     category: "electronics",
     description: "Keep track of your health and notifications with style.",
-    image: "https://via.placeholder.com/300x300?text=Watch",
+    image: "https://images.pexels.com/photos/18662969/pexels-photo-18662969.jpeg",
   },
 ];
 
